b9f83b8bbc5a5bcc10500ca6271e082d0a8e6486
AntonBarbakov <barbak2017@gmail.com>
2019-02-25 15:55:57 +0200

    v.0.0.1.1

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-     <div>
-       <header>
-         <img src="https://banner2.kisspng.com/20180420/hwq/kisspng-skull-and-bones-skull-and-crossbones-human-skull-s-5ad9c50dee7da9.6447893415242211979769.jpg" alt="skull"></img>
-       </header>
-        <nav>
-
+      <div className="app-wrapper">
+        <header className="header">
+          <img src="https://banner2.kisspng.com/20180420/hwq/kisspng-skull-and-bones-skull-and-crossbones-human-skull-s-5ad9c50dee7da9.6447893415242211979769.jpg" alt="skull"></img>
+        </header>
+        <nav className="nav">
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Messages</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Settings</a>
+          </div>
         </nav>
-        <div>
-          Main content
+        <div className="content">
+          <img src="https://cdn.playbuzz.com/cdn/b949b7bf-cb3d-41db-81c7-e6401d9cd4c9/004f8a04-7ca1-4211-8b51-0c51ef7fd706.jpg"></img>
         </div>
-     </div>
+      </div>
     );
   }
 }
