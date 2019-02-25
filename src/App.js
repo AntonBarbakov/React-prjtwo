488eb3e010ab58518cd1c813d4ab2f8f1a6d2647
AntonBarbakov <barbak2017@gmail.com>
2019-02-25 15:07:47 +0200

    v0.0.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+       <header>
+         <img src="https://banner2.kisspng.com/20180420/hwq/kisspng-skull-and-bones-skull-and-crossbones-human-skull-s-5ad9c50dee7da9.6447893415242211979769.jpg" alt="skull"></img>
+       </header>
+        <nav>
+
+        </nav>
+        <div>
+          Main content
+        </div>
+     </div>
     );
   }
 }
